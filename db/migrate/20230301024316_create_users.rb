b2af8ac4dc9e748a4b218965e4d83e049d6dff94
03040610 <yukikobayashi0304@gmail.com>
2023-03-01 12:12:09 +0900

    first_commit

---
+++ db/migrate/20230301024316_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email,               null: false
+      t.string :encrypted_password,  null: false
+      t.string :name,                null: false
+      t.text :profile,               null: false
+      t.text :occupation,            null: false
+      t.text :position,              null: false
+      t.timestamps
+
+    end
+  end
+end
+t.string :email,              null: false, default: ""
